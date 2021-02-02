afe005b6f9153cac472127e92932cacb5536498b
Luke Bray <lukisbray@gmail.com>
2021-02-01 17:42:47 -0800

    Full functioing front end

--- client/employees.js
+++ client/employees.js
@@ -13,10 +13,10 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["employeeId"] = document.getElementById("employeeId").value;
+    formData["fName"] = document.getElementById("fName").value;
+    formData["lName"] = document.getElementById("lName").value;
+    formData["bDate"] = document.getElementById("bDate").value;
     return formData;
 }
 
@@ -24,13 +24,13 @@ function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
+    cell1.innerHTML = data.employeeId;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.fName;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.lName;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
+    cell4.innerHTML = data.bDate;
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
@@ -38,8 +38,8 @@ function insertNewRecord(data) {
 
 function resetForm() {
     document.getElementById("employeeId").value = "";
-    document.getElementById("firstName").value = "";
-    document.getElementById("lastName").value = "";
+    document.getElementById("fName").value = "";
+    document.getElementById("lName").value = "";
     document.getElementById("bDate").value = "";
     selectedRow = null;
 }
@@ -47,15 +47,15 @@ function resetForm() {
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("employeeId").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("firstName").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("lastName").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("fName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("lName").value = selectedRow.cells[2].innerHTML;
     document.getElementById("bDate").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.employeeId;
+    selectedRow.cells[1].innerHTML = formData.fName;
+    selectedRow.cells[2].innerHTML = formData.lName;
+    selectedRow.cells[3].innerHTML = formData.bDate;
 }
 
 function onDelete(td) {
@@ -67,7 +67,7 @@ function onDelete(td) {
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("employeeId").value == "") {
         isValid = false;
         document.getElementById("fullNameValidationError").classList.remove("hide");
     } else {
