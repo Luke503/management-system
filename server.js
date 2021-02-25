fdcbfdeb010581fde442209b5caf31bac7c701ee
Luke Bray <lukisbray@gmail.com>
2021-02-25 15:08:39 -0800

    current

--- server.js
+++ server.js
@@ -1,70 +1,31 @@
+/*
+    Uses express, dbcon for database connection, body parser to parse form data
+    handlebars for HTML templates
+*/
+
 var express = require('express');
 var mysql = require('./dbcon.js');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
+var app = express();
+var handlebars = require('express-handlebars').create({
+        defaultLayout:'main',
+        });
 
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/departments.html'));
-});
-
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/employees.html'));
-});
-
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/positions.html'));
-});
-
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/projects.html'));
-});
-
-
-//add the router
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
 app.set('mysql', mysql);
-app.use('/', router);
-app.listen(process.env.port || 3000);
+app.use('/', express.static('public'));
 
-console.log('Running at Port 3000');
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
 
 
-
-// var app = express();
-// var handlebars = require('express-handlebars').create({
-//         defaultLayout:'main',
-//         });
-
-// app.engine('handlebars', handlebars.engine);
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use('/static', express.static('public'));
-// app.set('view engine', 'handlebars');
-// app.set('port', process.argv[2]);
-// app.set('mysql', mysql);
-// app.use('/people_certs', require('./people_certs.js'));
-// app.use('/people', require('./people.js'));
-// app.use('/planets', require('./planets.js'));
-// app.use('/', express.static('public'));
-
-// app.use(function(req,res){
-//   res.status(404);
-//   res.render('404');
-// });
-
-// app.use(function(err, req, res, next){
-//   console.error(err.stack);
-//   res.status(500);
-//   res.render('500');
-// });
-
-// app.listen(app.get('port'), function(){
-//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-// });
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
