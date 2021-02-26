e874c234bb22c3b0d2498063d368cb5f001c70ad
rajamong <rajamong@oregonstate.edu>
2021-02-25 18:44:51 -0800

    trying to get it to show up

--- server.js
+++ server.js
@@ -4,28 +4,34 @@
 */
 
 var express = require('express');
+var dbQueries = require('./backend/db')
 var mysql = require('./dbcon.js');
 var bodyParser = require('body-parser');
 
 var app = express();
 var handlebars = require('express-handlebars').create({
-        defaultLayout:'main',
-        });
+    defaultLayout: 'main',
+});
 
 app.engine('handlebars', handlebars.engine);
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/', express.static('public'));
 
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+app.get('/home', function(req, res, next) {
+    console.log("Using handlebars");
+    res.status(200).render('home');
+});
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
 });
 
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
