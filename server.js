237fc60b8ac4b5959d900b80153a9043c9c34f9b
Luke Bray <lukisbray@gmail.com>
2021-03-14 11:55:45 -0700

    progress
    progress

--- server.js
+++ server.js
@@ -1,31 +1,17 @@
-/*
-    Uses express, dbcon for database connection, body parser to parse form data
-    handlebars for HTML templates
-*/
+const express = require('express');
+const app = express();
+const expbs = require('express-handlebars');
 
-var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
 
-var app = express();
-var handlebars = require('express-handlebars').create({
-        defaultLayout:'main',
-        });
 
-app.engine('handlebars', handlebars.engine);
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/static', express.static('public'));
+app.engine('handlebars', expbs());
 app.set('view engine', 'handlebars');
-app.set('port', process.argv[2]);
-app.set('mysql', mysql);
-app.use('/', express.static('public'));
 
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(8080, () => {
+  console.log('Server is starting at port ', 8080);
 });
